97ecd10c17a0a9273294646545e50a3e87196a61
Mike Guzman <116609124+donmike99@users.noreply.github.com>
2023-07-09 10:23:46 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.send("Hello world!");
 });
 
 app.get("*", (req, res) => {
-  res.render("error404");
+  res.status(404).send("<h1>404 Page</h1>");
 });
 
 app.listen(process.env.PORT);
-
-app.listen(3006);
